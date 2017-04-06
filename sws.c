8049ac57ba1bd9ea89f1f899e0fd2627b7879866
jackieflash <jackie@jackieflash.com>
2017-04-06 09:48:58 -0300

    Refactoring

--- sws.c
+++ sws.c
@@ -23,61 +23,75 @@ int is_sjf = 0;
 int is_mlfb = 0;
 int is_rr = 0;
 
-pthread_t init_threads[NUM_THREADS];
-pthread_t client_serve_threads[NUM_THREADS];
 
 char alg_to_use[5];
 
-rcb *create_rcb(FILE *, int, char *);
+rcb *make_rbc(FILE *, int, char *);
 
-void lock_push(rcb *, heap_t *);
+void lock_enqueue(rcb *, heap_t *);
 
-void lock_push(rcb *, heap_t *);
+void lock_enqueue(rcb *, heap_t *);
 
 void *init_client(void *);
 
-void init();
+void rcb_init_t();
 
-rcb *pop_assign(int);
+rcb *lock_dequeue(int);
 
 void *work(void *);
 
+void algorithm_init(int *pInt, int *pInt1);
+
+void work_init_t(int *pInt);
+
 int main(int argc, char **argv) {
   int port = -1;
+  int num_threads = 64;
 
-  if ((argc < 3)
+
+  if ((argc < 4)
       || (sscanf(argv[1], "%d", &port) < 1)
-      || *(strcpy(alg_to_use, argv[2])) < 1) {
-    printf("usage: sws <port> <algorithm>\n");
+      || *(strcpy(alg_to_use, argv[2])) < 1
+      || sscanf(argv[3], "%d", &num_threads) < 1) {
+    printf("usage: sws <port> <algorithm> <thread number>\n");
     return 0;
   }
 
+  algorithm_init(&port, &num_threads);
+  network_init(port);
+  work_init_t(&num_threads);
+  return EXIT_SUCCESS;
+}
+
+void work_init_t(int *num_threads) {
+  int i;
+  pthread_t client_serve_threads[*num_threads];
+  for (i = 0; i < *num_threads; i++) {
+    pthread_create(&client_serve_threads[i], NULL, work, NULL);
+  }
+  rcb_init_t();
+  for (i = 0; i < *num_threads; i++) {
+    pthread_join(client_serve_threads[i], NULL);
+  }
+}
+
+void algorithm_init(int *port, int *num_threads) {
   is_sjf = !(strcmp(alg_to_use, "SJF") && strcmp(alg_to_use, "sjf"));
   is_rr = !(strcmp(alg_to_use, "RR") && strcmp(alg_to_use, "rr"));
   is_mlfb = !(strcmp(alg_to_use, "MLFB") && strcmp(alg_to_use, "mlfb"));
 
-  if (is_sjf) printf("Using shortest job first scheduling algorithm.\n");
-  else if (is_rr) printf("Using round robin scheduling algorithm.\n");
-  else if (is_mlfb) printf("Using multilevel feedback scheduling algorithm.\n");
+  if (is_sjf) printf("Using shortest job first scheduling algorithm ");
+  else if (is_rr) printf("Using round robin scheduling algorithm ");
+  else if (is_mlfb) printf("Using multilevel feedback scheduling algorithm ");
   else {
     printf("Not using a valid scheduling algorithm. Goodbye!\n");
     abort();
   }
-  network_init(port);
+  printf("with %d threads on port %d\n", *num_threads, *port);
 
-  int i;
-  for (i = 0; i < NUM_THREADS; i++) {
-    pthread_create(&client_serve_threads[i], NULL, work, NULL);
-  }
-  init();
-
-  for (i = 0; i < NUM_THREADS; i++) {
-    pthread_join(client_serve_threads[i], NULL);
-  }
-  return EXIT_SUCCESS;
 }
 
-rcb *create_rcb(FILE *fin, int fd, char *buffer) {
+rcb *make_rbc(FILE *fin, int fd, char *buffer) {
   rcb *new_rcb = (rcb *) malloc(sizeof(rcb));
   new_rcb->rcb_queue_level = 0;
   new_rcb->rcb_seq_num = counter; /* TODO: This may have to be fixed later */
@@ -85,28 +99,28 @@ rcb *create_rcb(FILE *fin, int fd, char *buffer) {
   new_rcb->rcb_serv_handle = fin;
   fseek(new_rcb->rcb_serv_handle, 0, SEEK_END);
 
-  int len_file = ftell(new_rcb->rcb_serv_handle);
+  int len = ftell(new_rcb->rcb_serv_handle);
   rewind(new_rcb->rcb_serv_handle);
   if (is_sjf) {
     new_rcb->rcb_quantum = MAX_HTTP_SIZE;
-    new_rcb->rcb_file_bytes_remain = len_file;
-    new_rcb->rcb_priority = len_file;
+    new_rcb->rcb_file_bytes_remain = len;
+    new_rcb->rcb_priority = len;
   }
   if (is_rr) {
     new_rcb->rcb_quantum = rr_quantum;
-    new_rcb->rcb_file_bytes_remain = len_file;
+    new_rcb->rcb_file_bytes_remain = len;
     new_rcb->rcb_priority = 1;
   }
-  if (strcmp(alg_to_use, "MLFB") == 0) {
+  if (is_mlfb) {
     rr_quantum = MAX_HTTP_SIZE; //TODO: make this not temporary
     new_rcb->rcb_quantum = max_queue_quantum;
-    new_rcb->rcb_file_bytes_remain = len_file;
+    new_rcb->rcb_file_bytes_remain = len;
     new_rcb->rcb_priority = 1;
   }
   return new_rcb;
 }
 
-void lock_push(rcb *new_rcb, heap_t *queue) {
+void lock_enqueue(rcb *new_rcb, heap_t *queue) {
   pthread_mutex_lock(&mutex);
   push(queue, new_rcb->rcb_priority, new_rcb);
   pthread_mutex_unlock(&mutex);
@@ -156,15 +170,15 @@ void *init_client(void *data) {
     } else {
       len = sprintf(buffer, "HTTP/1.1 200 OK\n\n");
       write(fd, buffer, len);
-      rcb *push_rcb = create_rcb(fin, fd, buffer);
-      lock_push(push_rcb, &max_queue);
+      rcb *push_rcb = make_rbc(fin, fd, buffer);
+      lock_enqueue(push_rcb, &max_queue);
     }
   }
   free(buffer);
   return 0;
 }
 
-void init() {
+void rcb_init_t() {
   int fd;
 
   for (;;) {
@@ -179,7 +193,7 @@ void init() {
   }
 }
 
-rcb *pop_assign(int len) {
+rcb *lock_dequeue(int len) {
   rcb *popped_rcb;
   pthread_mutex_lock(&mutex);
   if (max_queue.len > 0) {
@@ -209,7 +223,7 @@ void *work(void *data) {
   while (1) {
     int len = -1;
     //TODO: Fix this busy waiting
-    popped_rcb = pop_assign(len);
+    popped_rcb = lock_dequeue(len);
     if (popped_rcb) {
       if ((is_mlfb)) {
         if (popped_rcb->rcb_queue_level == 0) {
@@ -236,7 +250,7 @@ void *work(void *data) {
 
       if (is_rr) {
         if (popped_rcb->rcb_file_bytes_remain > popped_rcb->rcb_quantum) {
-          lock_push(popped_rcb, &max_queue);
+          lock_enqueue(popped_rcb, &max_queue);
         } else {
           close(popped_rcb->rcb_cli_desc);
           fclose(popped_rcb->rcb_serv_handle);
@@ -250,10 +264,10 @@ void *work(void *data) {
         if (len == max_queue_quantum || len == mid_queue_quantum) {
           if (popped_rcb->rcb_queue_level == 1) {
             popped_rcb->rcb_quantum = mid_queue_quantum;
-            lock_push(popped_rcb, &mid_queue);
+            lock_enqueue(popped_rcb, &mid_queue);
           } else if (popped_rcb->rcb_queue_level == 2) {
             popped_rcb->rcb_quantum = MAX_HTTP_SIZE;
-            lock_push(popped_rcb, &min_queue);
+            lock_enqueue(popped_rcb, &min_queue);
           }
         } else if (len < popped_rcb->rcb_quantum) {
           close(popped_rcb->rcb_cli_desc);
@@ -261,7 +275,6 @@ void *work(void *data) {
           free(popped_rcb);
         }
       }
-
     }
   }
 }
